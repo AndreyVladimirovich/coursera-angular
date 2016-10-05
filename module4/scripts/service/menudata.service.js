192feb3c6c6f11f992a55d8aa4a0ca46025f9140
topunov <topunov>
2016-10-05 22:57:26 +0300

    The first commit for module4 with scheleton

---
+++ module4/scripts/service/menudata.service.js
@@ -0,0 +1,14 @@
+
+(function () {
+'use strict';
+
+angular.module('Data')
+.service('MenuDataService', MenuDataService)
+
+MenuDataService.$inject = ['']
+function MenuDataService () {
+
+}
+
+
+})();
